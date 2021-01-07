042f07eeb3d1396d35b78f401387a7db121d4586
Nantarat Buranajinda <bron@Nantarats-MacBook-Pro-2.local>
2021-01-08 00:24:08 +0700

    finished testing

---
+++ SimpleSwiftFramework.framework/Headers/SimpleSwiftFramework.h
@@ -0,0 +1,18 @@
+//
+//  SimpleSwiftFramework.h
+//  SimpleSwiftFramework
+//
+//  Created by Nantarat Buranajinda on 7/1/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleSwiftFramework.
+FOUNDATION_EXPORT double SimpleSwiftFrameworkVersionNumber;
+
+//! Project version string for SimpleSwiftFramework.
+FOUNDATION_EXPORT const unsigned char SimpleSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleSwiftFramework/PublicHeader.h>
+
+
